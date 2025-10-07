296815f6c575147d7fa4df0c7cda389934f15edb
subodhteslatech <sibodh.teslatech@gmail.com>
2025-10-07 10:19:17 +0545

    feat: centrifugo client

--- socket_client.py
+++ socket_client.py
@@ -9,8 +9,9 @@ import websockets
 class CentrifugoClient:
     def __init__(self):
         self.websocket: websockets.ClientConnection | None = None
-        self.channel = None
+        self.channel = "chat:123"
         self.uri = "ws://localhost:8000/connection/websocket"
+        self.user_id = str(time.time())
 
     def _generate_token(self, user_id: str):
         payload = {"sub": user_id, "exp": int(time.time()) + 3600}
@@ -25,16 +26,24 @@ class CentrifugoClient:
             if self.websocket is None:
                 raise Exception("WebSocket is not connected")
 
+            print()
+            print()
+            print(f"User ID: {self.user_id}")
+            print()
+            print()
+
             await self.websocket.send(
                 json.dumps(
                     {
-                        "connect": {"token": self._generate_token(str(time.time()))},
+                        "connect": {"token": self._generate_token(self.user_id)},
                         "id": 1,
                     }
                 )
             )
             # response = await self.websocket.recv()
             # print(f"Response: {response}")
+
+            print("Connected to server")
         except Exception as e:
             print(f"Error: {e}")
 
@@ -48,48 +57,54 @@ class CentrifugoClient:
         if self.websocket is None:
             raise Exception("WebSocket is not connected")
 
-        channel = input("Enter channel to subscribe: ")
-        self.channel = channel
-
         await self.websocket.send(
             json.dumps(
                 {
-                    "subscribe": {"channel": channel},
+                    "subscribe": {"channel": self.channel},
                     "id": 2,
                 }
             )
         )
 
+        print("Subscribed to channel:", self.channel)
+
     async def publish(self):
-        if self.websocket is None:
-            raise Exception("WebSocket is not connected")
+        try:
+            if self.websocket is None:
+                raise Exception("WebSocket is not connected")
 
-        if self.channel is None:
-            raise Exception("No channel subscribed")
+            if self.channel is None:
+                raise Exception("No channel subscribed")
 
-        msg = input("Enter message to publish: ")
+            # msg = input("Enter message to publish: ")
+            msg = f"message from {self.user_id}"
 
-        await self.websocket.send(
-            json.dumps(
-                {
-                    "publish": {"channel": self.channel, "data": {"text": msg}},
-                    "id": 3,
-                }
+            await self.websocket.send(
+                json.dumps(
+                    {
+                        "publish": {"channel": self.channel, "data": {"text": msg}},
+                        "id": 3,
+                    }
+                )
             )
-        )
+        except Exception as e:
+            print(f"Error: {e}")
 
     async def listen_messages(self):
         if self.websocket is None:
             raise Exception("WebSocket is not connected")
 
-        await self.connect()
-        self.channel = "chat"
-        await self.subscribe()
+        # await self.connect()
+        # self.channel = "chat"
+        # await self.subscribe()
         try:
             while True:
                 print("Inside Block")
                 message = await self.websocket.recv()
                 print(f"Received message: {message}")
+
+                if message == "{}":
+                    await self.websocket.send(message)
         except Exception as e:
             print(f"Error: {e}")
 
@@ -98,32 +113,27 @@ class CentrifugoClient:
             async with websockets.connect(self.uri) as websocket:
                 self.websocket = websocket
                 asyncio.create_task(self.listen_messages())
-                await asyncio.sleep(3600)
+                # await asyncio.sleep(3600)
+                await self.connect()
+                await asyncio.sleep(1)
+                await self.subscribe()
+                await asyncio.sleep(1)
+                # await asyncio.sleep(3600)
                 while True:
                     try:
-                        print("1 - connect")
-                        print("2 - subscribe")
-                        print("3 - publish")
-                        print("4 - disconnect")
+                        await asyncio.sleep(5)
 
-                        event = input("Enter event: ")
+                        await self.publish()
 
-                        events = {
-                            "1": self.connect,
-                            "2": self.subscribe,
-                            "3": self.publish,
-                            "4": self.disconnect,
-                        }
-
-                        if event in events:
-                            await events[event]()
-                        else:
-                            print("Unknown event")
-
-                        # await self.listen_messages()
+                        await asyncio.sleep(10)
 
                     except websockets.ConnectionClosed:
+                        print("Connection closed, reconnecting...")
+                        await asyncio.sleep(5)
                         await self.disconnect()
+
+                    except Exception as e:
+                        print(f"Error during publish: {e}")
 
         except Exception as e:
             print(f"Error: {e}")
